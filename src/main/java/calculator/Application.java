8c6504d7386419dfbe91d8f7ea2216d16cca9058
ChangHyeon Jeon <ckdgus163@gmail.com>
2025-10-16 16:21:42 +0900

    feat: 쉼표 구분자를 기준으로 숫자 합산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 숫자를 입력하세요.");
+        String userInput = Console.readLine();
+
+        String[] userInputArr = userInput.split(",");
+        int sum = 0;
+        for (String numberString : userInputArr) {
+            int number = Integer.parseInt(numberString);
+            sum += number;
+        }
+        System.out.println(sum);
     }
 }
