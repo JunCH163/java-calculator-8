3a223d6ecfc30805065b7411b3c2d1bbca26c585
ChangHyeon Jeon <ckdgus163@gmail.com>
2025-10-17 18:15:42 +0900

    fix: null 또는 빈 문자열 입력 시 0을 반환하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,6 +9,10 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
+        if(userInput == null || userInput.isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
+        }
         userInput = userInput.replace("\\n", "\n");
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(userInput);
         int sum = 0;
